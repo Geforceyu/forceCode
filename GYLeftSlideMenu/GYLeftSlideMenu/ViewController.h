8c14a99274ace1f272494694b589266e53a3eced
Geforceyu <geforceyu@outlook.com>
2015-04-10 15:13:23 +0800

    no message

---
+++ GYLeftSlideMenu/GYLeftSlideMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GYLeftSlideMenu
+//
+//  Created by Geforceyu on 15/4/10.
+//  Copyright (c) 2015年 Geforceyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GYLeftSlideMenu/GYLeftSlideMenu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GYLeftSlideMenu
+//
+//  Created by Geforceyu on 15/4/10.
+//  Copyright (c) 2015年 Geforceyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
